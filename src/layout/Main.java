01a32bcbb974e39f2f0ae4fcf9ca191ada372eb8
John P <jpigottmail@gmail.com>
2015-12-30 13:36:10 -0500

    Fixed layout cells not filling area
    Fixed the bug in issue #16 regarding the JPanels not taking up all the
    variable space

--- src/layout/Main.java
+++ src/layout/Main.java
@@ -16,10 +16,10 @@ public class Main extends JFrame
 
     //panel options
     private int graphRows = gridRows;
-    private int graphCols = 3;
+    private int graphCols = 4;
 
-    private int optionRows = graphRows;
-    private int optionCols = gridCols - graphCols; //we want the options to fill whatever space the graph doesn't
+    private int optionRows = gridRows;
+    private int optionCols = 1; //we want the options to fill whatever space the graph doesn't
 
     public Main()
     {
@@ -35,7 +35,7 @@ public class Main extends JFrame
         InitPanels(mainPanel);
 
         //finally add the main panel
-        this.setLayout(new FlowLayout());
+        this.setLayout(new GridLayout(1, 1));
         this.add(mainPanel);
 
     }
@@ -47,8 +47,6 @@ public class Main extends JFrame
 
         //set up the constraints common to both
         GridBagConstraints constraints = new GridBagConstraints();
-        constraints.weightx = 1;
-        constraints.weighty = 1;
         constraints.fill = GridBagConstraints.BOTH;
 
         //graph properties
@@ -57,16 +55,21 @@ public class Main extends JFrame
         constraints.gridy = 0;
         constraints.gridheight = graphRows;
         constraints.gridwidth = graphCols;
+        constraints.weightx = .75;
+        constraints.weighty = 1;
 
         //add graph panel
         parent.add(graph, constraints);
 
         //set the specific values for options
         constraints.anchor = GridBagConstraints.WEST;
-        constraints.gridx = gridCols - graphCols;
-        constraints.gridy = gridRows - graphRows;
+        constraints.gridx = 4;
+        constraints.gridy = 0;
         constraints.gridheight = optionRows;
         constraints.gridwidth = optionCols;
+        constraints.weightx = .25;
+        constraints.weighty = 1;
+
 
         //add the options panel
         parent.add(options, constraints);
